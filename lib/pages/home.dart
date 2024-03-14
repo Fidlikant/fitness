aa07e0963d9fb62f3a4cc3df142b59f140bd6ef2
Filip Šmehyl <filip.smehyl20@gmail.com>
2024-03-14 22:48:31 +0100

    pridal som na hladacik svgčka a napovedu co hladat

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,31 +7,47 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBar(),
-      body: Column(
-        children: [
-          _searchField()
-          ]
-        
-      )
-
-    );
+        appBar: appBar(),
+        body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          _searchField(),
+          SizedBox(height: 40),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Text(
+                  'Category',
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(height: 15),
+              Container(
+                height: 100,
+                color: Colors.green,
+                
+              )
+            ],
+          )
+        ]));
   }
 
   Container _searchField() {
     return Container(
-          margin: const EdgeInsets.only(top: 60, left: 40, right: 40),
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Color.fromARGB(255, 60, 58, 59).withOpacity(0.11),
-                blurRadius: 20,
-                spreadRadius: 0.0,
-              )
-            ]
-          ),
-          child: TextField(
-            decoration: InputDecoration(
+        margin: const EdgeInsets.only(top: 60, left: 40, right: 40),
+        decoration: BoxDecoration(boxShadow: [
+          BoxShadow(
+            color: Color.fromARGB(255, 60, 58, 59).withOpacity(0.11),
+            blurRadius: 20,
+            spreadRadius: 0.0,
+          )
+        ]),
+        child: TextField(
+          decoration: InputDecoration(
             filled: true,
             fillColor: Color.fromARGB(255, 255, 255, 255),
             contentPadding: const EdgeInsets.all(15),
@@ -40,7 +56,7 @@ class HomePage extends StatelessWidget {
               child: SvgPicture.asset('assets/icons/Search.svg'),
             ),
             hintText: 'Search for something good...',
-              hintStyle: TextStyle(
+            hintStyle: TextStyle(
               color: Colors.grey,
               fontSize: 12,
             ),
@@ -49,40 +65,33 @@ class HomePage extends StatelessWidget {
               child: SvgPicture.asset('assets/icons/Filter.svg'),
             ),
             border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(15),
-              borderSide: BorderSide.none
-            ),
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide.none),
           ),
-        )
-        );
+        ));
   }
 
   AppBar appBar() {
     return AppBar(
-      title: const Text('Raňajky', style: TextStyle(color: Color.fromARGB(255, 0, 0, 0))),
+      title: const Text('Raňajky',
+          style: TextStyle(color: Color.fromARGB(255, 0, 0, 0))),
       centerTitle: true,
       backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       leading: GestureDetector(
-        onTap:() {
-          
-        },
-      child: Container(
-        margin: const EdgeInsets.all(10),
-        alignment: Alignment.center,
-        child: SvgPicture.asset('assets/icons/Arrow - Left 2.svg'),
-        decoration: BoxDecoration(
-            
-            borderRadius: BorderRadius.circular(10)),
-      ),
+        onTap: () {},
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          child: SvgPicture.asset('assets/icons/Arrow - Left 2.svg'),
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+        ),
       ),
       actions: [
         Container(
           margin: const EdgeInsets.all(10),
           alignment: Alignment.center,
           child: SvgPicture.asset('assets/icons/dots.svg'),
-          decoration: BoxDecoration(
-              
-              borderRadius: BorderRadius.circular(10)),
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
         ),
       ],
     );
