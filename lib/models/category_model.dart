3b0bccc8125adc44fdd38a8543814ed6833b05bd
Filip Šmehyl <filip.smehyl20@gmail.com>
2024-03-15 00:18:41 +0100

    zmenil som nazov appky a pridal som kategorie obcerstvenia(zoznam)

---
+++ lib/models/category_model.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class CategoryModel {
+  String name;
+  String iconPath;
+  Color boxColor;
+
+  CategoryModel(
+    {
+      required this.name, 
+      required this.iconPath, 
+      required this.boxColor,
+    }
+    );
+
+    static List<CategoryModel> getCategories() {
+
+      List<CategoryModel> categories = [];
+
+      categories.add(CategoryModel(
+        name: 'Salad',
+        iconPath: 'assets/icons/plate.svg',
+        boxColor: Colors.green,
+      ));
+
+      categories.add(CategoryModel(
+        name: 'Cake',
+        iconPath: 'assets/icons/pancakes.svg',
+        boxColor: Colors.blue,
+      ));
+
+      categories.add(CategoryModel(
+        name: 'Pie',
+        iconPath: 'assets/icons/pie.svg',
+        boxColor: Color.fromARGB(255, 148, 41, 110),
+      ));
+
+      categories.add(CategoryModel(
+        name: 'Smoothies',
+        iconPath: 'assets/icons/orange-snacks.svg',
+        boxColor: Color.fromARGB(255, 230, 138, 0),
+      ));
+
+      categories.add(CategoryModel(
+        name: 'Salmon of Nigiri',
+        iconPath: 'assets/icons/salmon-nigiri.svg',
+        boxColor: Color.fromARGB(255, 255, 0, 247),
+      ));
+
+      categories.add(CategoryModel(
+        name: 'Canaian Bread',
+        iconPath: 'assets/icons/canai-bread.svg',
+        boxColor: Color.fromARGB(255, 157, 60, 0),
+      ));
+
+
+
+      return categories;
+    }
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,11 +1,24 @@
+import 'package:fitness/models/category_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<CategoryModel> categories = [];
+
+  void _getCategories() {
+    categories = CategoryModel.getCategories();
+  }
 
   @override
   Widget build(BuildContext context) {
+    _getCategories();
     return Scaffold(
         appBar: appBar(),
         body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
@@ -27,10 +40,47 @@ class HomePage extends StatelessWidget {
               ),
               SizedBox(height: 15),
               Container(
-                height: 100,
-                color: Colors.green,
-                
-              )
+                height: 150,
+                child: ListView.separated(
+                  itemCount: categories.length,
+                  scrollDirection: Axis.horizontal,
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  separatorBuilder: (context, index) => SizedBox(width: 20),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      width: 100,
+                      decoration: BoxDecoration(
+                        color: categories[index].boxColor.withOpacity(0.3),
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                            width: 50,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 255, 255, 255),
+                              shape: BoxShape.circle,
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: SvgPicture.asset(categories[index].iconPath),
+                              ),
+                          ),
+                          Text(
+                            categories[index].name,
+                            style: const TextStyle(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ]),
+                    );
+                  },
+                ),
+              ),
             ],
           )
         ]));
